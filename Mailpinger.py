e0f686cf1ef67e7b42b9545db7fae3bf8748111c
thego0n <55830543+thego0n@users.noreply.github.com>
2021-01-17 05:44:53 +0000

    Update Mailpinger.py

--- Mailpinger.py
+++ Mailpinger.py
@@ -7,9 +7,9 @@ port = 465
 count = 0
 def SENDY():
     sent_from = gmail_user
-    to = ['mail@mail.xom']
-    subject = 'justa subject'
-    body = '///////////add my spiel here////'
+    to = ['YOURTARGET@MAIL.com']
+    subject = 'YOUR SUBJECT'
+    body = '////ADD YOUR MESSAGE HERE (HTML IS POSSIBLE)///'
     email_text = """\
     From: %s
     To: %s
@@ -22,9 +22,9 @@ def SENDY():
         server.login(gmail_user, gmail_password)
         server.sendmail(sent_from, to, email_text)
         server.close()
-        print('one step closer to heaven baby')
+        print('email sent - all is well..NEXT!')
     except:
         print('Something went wrong...')
-while count < 1000000:
+while count < 301:
     SENDY()
     count += 1  # count = count + 1
